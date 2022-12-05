5c43c553dd6d14b8c5281301abb71ffeff6f6788
Nikolay Sergushenkov <n.sergushenkov@tinkoff.ru>
2022-12-05 11:48:14 +0300

    day 05

---
+++ 5/d05_1.py
@@ -0,0 +1,49 @@
+import re
+
+input = '''    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+'''
+
+with open('input.txt', 'r') as file:
+    input = file.read()
+
+row_count = 0
+rows = input.split('\n')
+
+for line in rows:
+    row_count += 1    
+    if not (line.strip()[0].isdigit()):
+        continue
+    stack_count = len(line.strip().split('   '))
+    break
+
+stacks = []
+for i in range(stack_count):
+    stacks.append([])
+
+for line in rows[row_count-2::-1]:
+    stack = 0
+    while stack < stack_count:
+        if line[4*stack] == '[':
+            crate = line[4*stack + 1]
+            stacks[stack].append(crate)
+        stack += 1
+
+pattern = r'(move | from | to )'
+for line in rows[row_count + 1 : ]:
+    if line == '':
+        break
+    _, move, _, from_stack, _, to_stack = line.split(' ')
+    for i in range(int(move)):
+        crate = stacks[int(from_stack) - 1].pop()
+        stacks[int(to_stack) - 1].append(crate)
+
+for stack in stacks:
+    print(stack.pop(), end='')
