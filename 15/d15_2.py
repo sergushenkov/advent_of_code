6f0ddba2fe5582f610bb3d2d9c8da2dd33e3b224
sergushenkov <sergushenkov@gmail.com>
2022-12-15 14:22:14 +0300

    day 15

--- 15/d15_2.py
+++ 15/d15_2.py
@@ -27,22 +27,30 @@ for line in input_string.split('\n'):
     taxicab_length = abs(beacon_x - sensor_x) + abs(beacon_y - sensor_y)
     input_data.append((sensor_x, sensor_y, beacon_x, beacon_y, taxicab_length))
 
+# max_x = 20 + 1
+# max_y = 20 + 1
 max_x = 4_000_000 + 1
 max_y = 4_000_000 + 1
-grid = [['.' for y in range(max_y)] for x in range(max_x)]
+grid = [set() for x in range(max_x)]
 for (sensor_x, sensor_y, beacon_x, beacon_y, taxicab_length) in input_data:
-    if 0 < sensor_x < max_x and 0 < sensor_y < max_y:
-        grid[sensor_x][sensor_y] = 'S'
-    if 0 < beacon_x < max_x and 0 < beacon_y < max_y:
-        grid[beacon_x][beacon_y] = 'B'
     for x in range(max(0, sensor_x - taxicab_length), min(max_x, sensor_x + taxicab_length)):
-        for y in range(max(0, sensor_y - taxicab_length), min(max_y, sensor_y + taxicab_length)):
-            if grid[x][y] in {'S', 'B', '#'}:
-                continue
-            if abs(x - sensor_x) + abs(y - sensor_y) <= taxicab_length:
-                grid[x][y] = '#'
+        delta_y = taxicab_length - abs(x - sensor_x)
+        from_y = max(0, sensor_y - delta_y)
+        to_y = min(max_y, sensor_y + delta_y)
+        grid[x].add((from_y, to_y))
 
 for x in range(max_x):
-    for y in range(max_y):
-        if grid[x][y] == '.':
-            print(x * 4_000_000 + y)
+    y = 0
+    flag = True
+    while y < max_y and flag:
+        flag = False
+        for (from_y, to_y) in grid[x]:
+            if from_y <= y < to_y or (y > 0 and y + 1 == from_y and y < to_y):
+                y = to_y
+                flag = True
+                break
+        if y == max_y:
+            flag = True
+    if not flag:
+        print(x, y+1, x * 4_000_000 + y+1)
+        exit()
